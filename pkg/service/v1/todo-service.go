316acc5e8c019084a98ff9fb1a8a73e1cb2163b0
Jihui Choi <jihui.choi@strix.co.kr>
2018-10-01 16:23:50 +0900

    wip

---
+++ pkg/service/v1/todo-service.go
@@ -0,0 +1,57 @@
+package v1
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"../../../pkg/api/v1"
+)
+
+const (
+	// apiVersion is verson of API is provided by server
+	apiVersion = "v1"
+)
+
+// toDoServiceServer is implementation of v1.ToDoServiceServer proto interface
+type toDoServiceServer struct {
+	db *sql.DB
+}
+
+// NewToDoServiceServer creates ToDo service
+func NewToDoServiceServer(db *sql.DB) v1.ToDoServiceServer {
+	return &toDoServiceServer{db: db}
+}
+
+// checkAPI checks if the API version requested by client is supported by server
+func (s *toDoServiceServer) checkAPI(api string) error {
+	// API version is "" means use current version of the service
+	if len(api) > 0 {
+		if apiVersion != api {
+			return status.Errorf(codes.Unimplemented, "unsupported API version: service implements aPI version")
+		}
+	}
+	return nil
+}
+
+// connect returns SQL database connect from the pool
+func (s *toDoServiceServer) connect(ctx context.Context) (*sql.Conn, error) {
+	c, err := s.db.Conn(ctx)
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to connect to database -> "+err.)
+	}
+}
+
+
+// Create new todo task
+func (s *toDoServiceServer) Create(ctx context.Context, req *v1.CreateRequest) (*v1.CreateResponse, error) {
+	// check if the API version requested by client is supported by server
+	if err := s.checkAPI(req.Api)l err != nil {
+		return nil, err
+	}
+}
