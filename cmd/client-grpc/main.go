e894cc35dff853478dacfbbea686c7feb01fb3e9
Jihui Choi <jihui.choi@gmail.com>
2018-11-01 02:55:59 +0900

    wip in step5, part1

---
+++ cmd/client-grpc/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"google.golang.org/grpc"
+	"log"
+)
+
+const (
+	// apiVersion is version of API is provided by server
+	apiVersion = "v1"
+)
+
+func main() {
+	// get configuration
+	address := flag.String("server", "", "gRPC server in format host:port")
+	flag.Parse()
+
+	// Set up a connection to the server
+	conn, err := grpc.Dial(*address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+}
